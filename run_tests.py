981ed623a654b22951773eb8835f5c285e19590b
Callum Donaldson <callumdonaldson1990@gmail.com>
2022-08-03 11:30:56 +0100

    all classes and tests created

--- run_tests.py
+++ run_tests.py
@@ -1,5 +1,8 @@
 import unittest
 from tests.pub_test import TestPub
+from tests.customer_test import TestCustomer
+from tests.drink_test import TestDrink
+
 # from the pub_test in the tests folder, we are importing the class TestPub
 
 if __name__ == '__main__':
